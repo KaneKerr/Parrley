405101f57d6ac7f35ba8df79d920f1d52cd5e09f
Kane Kerr <Kanekerr6@mail.com>
2024-12-01 12:24:29 +0000

    Function to call and load json with orders, if it does not exist, do nothing else start monitoring

--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
-from newsFilter import get_news
+from apiLogic import monitor_price
+from state_manager import load_order_state, order_state
+import threading
 import time
-from botLogic import daily_candles
+from newsFilter import get_news
+
+
+def resume_monitoring():
+    for order_id, trade_data in order_state.items():
+        monitoring_thread = threading.Thread(
+            target=monitor_price,
+            args=(
+                order_id,
+                trade_data["lot"],
+                trade_data["take"],
+                trade_data["stop"],
+                None
+            )
+        )
+        monitoring_thread.daemon = True
+        monitoring_thread.start()
+        print(f"Resumed monitoring for Order ID: {order_id}")
 
 
 def main():
+    load_order_state()
+    resume_monitoring()
+
     while True:
         get_news()
         time.sleep(3600)
